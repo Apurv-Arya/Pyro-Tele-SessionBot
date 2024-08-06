a640acdf101a954d7b42a480f5d8c3daa74f4665
Apurv Arya <apurvarya19@gmail.com>
2024-08-06 21:02:37 +0530

    sa

--- env.py
+++ env.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "28611965").strip()
 API_HASH = os.getenv("API_HASH", "d36cfa9250dd3d0d46678b538836ca8b").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7270342784:AAHEzCyWZ4xyBpja1Z9ABBOhlKcCXL02jgs").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://BinaryBandiT69:BinaryBandiT69@db9.cj2gfsp.mongodb.net/?retryWrites=true&w=majority&appName=DB9").strip() # Not a necessary variable anymore but you can add to get stats
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
 MUST_JOIN = os.getenv("MUST_JOIN", "TBots_Father")
 
 if not API_ID:
